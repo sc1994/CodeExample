72f6b70d1e6def555a24d9cce7bd046f6165dcda
suncheng <sunchengsuncheng@gmail.com>
2018-01-27 01:30:18 +0800

    add expressionhelper

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,44 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using Cache;
 using Entity;
+using Utilities;
 
 namespace Test
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			//var model = new Person
-			//{
-			//    Id = Guid.NewGuid(),
-			//    FirstName = "sun",
-			//    LastName = "cheng"
-			//};
-			//var result = CacheProvider.RedisDefault.Set(CacheKeys.DefaultKey, model);
-			//var result = CacheProvider.RedisDefault.Get<Person>(CacheKeys.DefaultKey);
-			var hash = new List<Person>
-					   {
-						   new Person
-						   {
-							   Id = Guid.NewGuid(),
-							   FirstName = "sun",
-							   LastName = "cheng"
-						   },
-						   new Person
-						   {
-							   Id = Guid.NewGuid(),
-							   FirstName = "sun2",
-							   LastName = "cheng2"
-						   },
-						   new Person
-						   {
-							   Id = Guid.NewGuid(),
-							   FirstName = "sun3",
-							   LastName = "cheng3"
-						   }
-					   };
-			CacheProvider.RedisDefault.SetHash(CacheKeys.DefaultHashtKey, hash, person => person.Id.ToString());
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<Person, bool>> first =
+                x => x.FirstName == "sun";
+
+            first = first.And(x => x.LastName == "cheng");
+
+            Console.WriteLine(first.Body.ToString());
+            //var model = new Person
+            //{
+            //    Id = Guid.NewGuid(),
+            //    FirstName = "sun",
+            //    LastName = "cheng"
+            //};
+            //var result = CacheProvider.RedisDefault.Set(CacheKeys.DefaultKey, model);
+            //var result = CacheProvider.RedisDefault.Get<Person>(CacheKeys.DefaultKey);
+            //var hash = new List<Person>
+            //		   {
+            //			   new Person
+            //			   {
+            //				   Id = Guid.NewGuid(),
+            //				   FirstName = "sun",
+            //				   LastName = "cheng"
+            //			   },
+            //			   new Person
+            //			   {
+            //				   Id = Guid.NewGuid(),
+            //				   FirstName = "sun2",
+            //				   LastName = "cheng2"
+            //			   },
+            //			   new Person
+            //			   {
+            //				   Id = Guid.NewGuid(),
+            //				   FirstName = "sun3",
+            //				   LastName = "cheng3"
+            //			   }
+            //		   };
+            //CacheProvider.RedisDefault.SetHash(CacheKeys.DefaultHashtKey, hash, person => person.Id.ToString());
+        }
+    }
 }
